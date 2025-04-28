195472a5eb3d952d448915a7abcf5feaf91d9a0d
Eric Rizzi <eric.rizzi@gmail.com>
2025-04-28 07:27:57 -0500

    Update warm up problem to new version of Prisoner's dilemma

--- CourseMaterial/03_programming_in_practice/08_prisoners_lists/prisoners_warm_up.py
+++ CourseMaterial/03_programming_in_practice/08_prisoners_lists/prisoners_warm_up.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-def prisoner_bot(my_choices, other_choices):
-    if len(my_choices) == 0:
+def prisoner_bot(other_choices):
+    if len(other_choices) == 0:
         return "defect"
     elif "defect" in other_choices:
         return "defect"
@@ -10,8 +10,8 @@ def prisoner_bot(my_choices, other_choices):
 
 if __name__ == "__main__":
     # Round 1
-    round_1 = prisoner_bot([], [])
+    round_1 = prisoner_bot([])
     print(round_1)
     # Round 2
-    round_2 = prisoner_bot(["defect"], ["cooperate"])
+    round_2 = prisoner_bot(["cooperate"])
     print(round_2)
